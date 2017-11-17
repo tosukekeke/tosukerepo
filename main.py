75e7e77ad4f515a39077f1b861c7c7f048780f8e
加藤佑介 <katouyuusuke@katouyuusukenoMacBook-Air.local>
2017-11-17 23:42:03 +0900

    miner change
    miss!!

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 def main () :
-  a,b =  [int(i) for i in input().split()]
-    if a<b:
-        print(a<b)
-    elif a>b :
-        print(a>b)
+    a, b = [int(i) for i in input().split()]
+    if a < b :
+        print( "a < b")
+    elif a > b :
+        print("a > b")
     else :
-        print(a==b)
+        print("a == b")
 
-if__name__=='__main__':
-main()
+if __name__=='__main__':
+    main()
